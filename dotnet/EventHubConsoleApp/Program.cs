edfe2a74f3007c7dfcaf3351d4adf4c83ac1bf34
Chris Joakim <christopher.joakim@gmail.com>
2020-07-02 13:06:55 -0400

    Starting dotnet/EventHubConsoleApp

---
+++ dotnet/EventHubConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventHubConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
